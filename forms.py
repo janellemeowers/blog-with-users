a4ee88281cf2aea1f5edb34173c192562f019eab
janelle <janelle.myers90@gmail.com>
2022-12-20 19:00:17 -0800

    update forms class

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField,
-from wtforms.validators import DataRequired, URL,
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
 ##WTForm
